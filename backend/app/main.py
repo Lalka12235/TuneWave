c3d5138a464b0831c277a4e17e63b89fa531595e
Egor <fisher12235@mail.ru>
2025-04-01 10:22:06 +0700

    init

---
+++ backend/app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title='TuneWave',
+    description='Trackify - is your personal music collection where every track and playlist has its place. Easily add, \
+        update and share music with friends. Manage your music world wherever you are.'
+)
+
