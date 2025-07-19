c9d8f71dfe57454c078283592e77863d3d3e2ae2
Egor <fisher12235@mail.ru>
2025-07-19 13:52:14 +0700

    New start projec

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,5 @@
-#from .user import UserModel
-#from .room import RoomModel, RoomMembersModel
-#from .track import TrackModel, RoomTracksModel
-#from app.models.base import Base  # <-- Убедись, что Base импортируется сюда!
-#
-#__all__ = ["UserModel", "RoomModel", "RoomMembersModel", "TrackModel", "RoomTracksModel", "Base"]
+__all__ = (
+    'Base',
+)
+
+from app.models.base import Base
