4f33c892769ba4ee8d292743377ca6b6fadfc2d1
Egor <fisher12235@mail.ru>
2025-07-27 18:10:08 +0700

    room repository

---
+++ backend/app/repositories/room.py
@@ -0,0 +1,112 @@
+from app.models.room import Room
+from sqlalchemy import select,delete
+from sqlalchemy.orm import Session
+import uuid
+from typing import Any
+
+
+class RoomRepository:
+    """
+    Репозиторий для выполнения операций CRUD (Create, Read, Update, Delete)
+    над моделью Room в базе данных.
+    """
+
+    @staticmethod
+    def get_room_by_id(db: Session, room_id: uuid.UUID) -> Room | None:
+        """
+        Получает комнату по ее уникальному идентификатору (ID).
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            room_id (uuid.UUID): ID комнаты для поиска.
+
+        Returns:
+            Room | None: Объект комнаты, если найден, иначе None.
+        """
+        stmt = select(Room).where(Room.id == room_id)
+        result = db.execute(stmt)
+        return result.scalar_one_or_none()
+    
+    @staticmethod
+    def get_room_by_name(db: Session, name: str) -> Room | None:
+        """
+        Получает комнату по ее названию.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            name (str): Название комнаты для поиска.
+
+        Returns:
+            Room | None: Объект комнаты, если найден, иначе None.
+        """
+        stmt = select(Room).where(Room.name == name)
+        result = db.execute(stmt)
+        return result.scalar_one_or_none()
+
+    @staticmethod
+    def get_all_rooms(db: Session) -> list[Room]:
+        """
+        Получает список всех комнат из базы данных.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+
+        Returns:
+            List[Room]: Список объектов Room.
+        """
+        stmt = select(Room)
+        result = db.execute(stmt)
+        return result.scalars().all()
+    
+
+
+    @staticmethod
+    def create_room(db: Session,room_data: dict[str, Any]) -> Room:
+        """
+        Создает новую комнату в базе данных.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            room_data (Dict[str, Any]): Словарь с данными для создания комнаты.
+                                        Должен содержать поля, соответствующие модели Room.
+
+        Returns:
+            Room: Созданный объект комнаты.
+        """
+        new_room = Room(**room_data)
+        db.add(new_room)
+        return new_room
+    
+    @staticmethod
+    def update_room(db: Session,room: Room, update_data: dict[str,Any]) -> Room:
+        """
+        Обновляет существующую комнату в базе данных.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            room (Room): Объект комнаты, который нужно обновить.
+            update_data (Dict[str, Any]): Словарь с данными для обновления.
+
+        Returns:
+            Room: Обновленный объект комнаты.
+        """
+        for key,value in update_data.items():
+            setattr(room,key,value)
+        return room
+
+
+    @staticmethod
+    def delete_room(db: Session, room_id: uuid.UUID) -> bool:
+        """
+        Удаляет комнату из базы данных по ее ID.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            room_id (uuid.UUID): ID комнаты для удаления.
+
+        Returns:
+            bool: True, если комната была успешно удалена, иначе False.
+        """
+        stmt = delete(Room).where(Room.id == room_id)
+        result = db.execute(stmt)
+        return result.rowcount > 0
