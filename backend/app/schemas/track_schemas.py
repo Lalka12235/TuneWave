e69ffb8df2d665424400a2eb24fbe0b50d8a06a5
Egor <fisher12235@mail.ru>
2025-07-31 10:42:20 +0700

    track schemas

---
+++ backend/app/schemas/track_schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field, ConfigDict
+import uuid
+
+
+class TrackBase(BaseModel):
+    spotify_id: str = Field(..., description="ID трека на Spotify")
+    title: str = Field(..., description="Название трека")
+    artist: str = Field(..., description="Исполнитель трека")
+    album: str = Field(..., description="Название альбома")
+    album_image_url: str | None = Field(None, description="URL обложки альбома")
+    duration_ms: int = Field(..., description="Длительность трека в миллисекундах")
+    external_url: str | None = Field(None, description="Ссылка на трек на Spotify")
+
+class TrackCreate(TrackBase):
+    pass
+
+class TrackResponse(TrackBase):
+    id: uuid.UUID = Field(..., description="Уникальный ID трека в вашей базе данных")
+    created_at: str = Field(..., description="Дата и время создания записи")
+    updated_at: str | None = Field(None, description="Дата и время последнего обновления записи")
+
+    model_config = ConfigDict(from_attributes=True)
