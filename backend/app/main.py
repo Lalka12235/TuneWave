3211aa2de44d63885adaf0cc10dc618739abd2b3
Egor <fisher12235@mail.ru>
2025-08-06 11:34:11 +0700

    .

--- backend/app/main.py
+++ backend/app/main.py
@@ -62,15 +62,15 @@ async def ping():
     return 'Server is running'
 
 
-origins = [
-    "http://localhost",  
-    "http://localhost:8080",
-    "http://127.0.0.1:5500",#test
-]
+#origins = [
+#    "http://localhost",  
+#    "http://localhost:8080",
+#    "http://127.0.0.1:5500",#test
+#]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],  
