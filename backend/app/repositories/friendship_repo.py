fc29d9ae3c59d513cd2b6588daa02c48111d57c7
Egor <fisher12235@mail.ru>
2025-08-13 15:18:19 +0700

    friendship repo

---
+++ backend/app/repositories/friendship_repo.py
@@ -0,0 +1,197 @@
+from sqlalchemy import select,delete,func,or_,and_,update
+from sqlalchemy.orm import Session,joinedload
+from backend.app.models.friendship import Friendship
+import uuid
+from app.schemas.enum import FriendshipStatus
+from datetime import datetime
+
+
+class FriendshipRepository:
+    """
+    Репозиторий для выполнения операций CRUD над моделью Friendship.
+    Отвечает за управление записями о дружбе и запросах на дружбу.
+    """
+
+    @staticmethod
+    def get_friendship_by_id(db: Session,friendship_id: uuid.UUID) -> Friendship | None:
+        """
+        Получает запись о дружбе по её ID.
+        Загружает отношения requester и accepter для удобства.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            friendship_id (uuid.UUID): ID записи о дружбе.
+
+        Returns:
+            Optional[Friendship]: Объект Friendship или None, если не найден.
+        """
+        stmt = select(Friendship).where(
+            Friendship.id == friendship_id,
+        ).options(
+            joinedload(Friendship.requester),
+            joinedload(Friendship.accepter),
+        )
+        result = db.execute(stmt)
+        return result.scalar_one_or_none()
+    
+
+    @staticmethod
+    def get_friendship_by_users(db: Session,user1_id: uuid.UUID,user2_id: uuid.UUID) -> Friendship | None:
+        """
+        Получает запись о дружбе между двумя конкретными пользователями,
+        независимо от того, кто отправитель, а кто получатель.
+        Загружает отношения requester и accepter.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            user1_id (uuid.UUID): ID первого пользователя.
+            user2_id (uuid.UUID): ID второго пользователя.
+
+        Returns:
+            Optional[Friendship]: Объект Friendship (если существует), иначе None.
+        """
+        stmt = select(Friendship).where(
+            or_(
+                and_(Friendship.requester_id == user1_id, Friendship.accepter_id == user2_id),
+                and_(Friendship.requester_id == user2_id, Friendship.accepter_id == user1_id)
+            )
+        ).options(
+            joinedload(Friendship.requester),
+            joinedload(Friendship.accepter),
+        )
+        result = db.execute(stmt)
+        return result.scalar_one_or_none()
+    
+
+    @staticmethod
+    def get_user_friends(db: Session,user_id: uuid.UUID) -> list[Friendship]:
+        """
+        Получает список всех принятых друзей для указанного пользователя.
+        Загружает отношения requester и accepter.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            user_id (uuid.UUID): ID пользователя, для которого ищутся друзья.
+
+        Returns:
+            List[Friendship]: Список объектов Friendship со статусом ACCEPTED.
+        """
+        stmt = select(Friendship).where(
+            Friendship.status == FriendshipStatus.ACCEPTED,
+            or_(
+                Friendship.requester_id == user_id,
+                Friendship.accepter_id == user_id,
+            )
+        ).options(
+            joinedload(Friendship.requester),
+            joinedload(Friendship.accepter),
+        )
+        result = db.execute(stmt)
+        return result.scalars().all()
+    
+
+    @staticmethod
+    def get_sent_requests(db: Session,requester_id: uuid.UUID) -> list[Friendship]:
+        """
+        Получает список всех запросов на дружбу, отправленных указанным пользователем,
+        которые находятся в статусе PENDING.
+        Загружает отношение accepter.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            requester_id (uuid.UUID): ID пользователя, который отправил запросы.
+
+        Returns:
+            List[Friendship]: Список объектов Friendship со статусом PENDING.
+        """
+        stmt = select(Friendship).where(
+            Friendship.requester_id == requester_id,
+            Friendship.status == FriendshipStatus.PENDING,
+        ).options(
+            joinedload(Friendship.accepter),
+        )
+        result = db.execute(stmt)
+        return result.scalars().all()
+    
+
+    @staticmethod
+    def get_received_requests(db: Session,accepter_id: uuid.UUID) -> list[Friendship]:
+        """
+        Получает список всех запросов на дружбу, полученных указанным пользователем,
+        которые находятся в статусе PENDING.
+        Загружает отношение requester.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            accepter_id (uuid.UUID): ID пользователя, который получил запросы.
+
+        Returns:
+            List[Friendship]: Список объектов Friendship со статусом PENDING.
+        """
+        stmt = select(Friendship).where(
+            Friendship.status == FriendshipStatus.PENDING,
+            Friendship.accepter_id == accepter_id,
+        ).options(
+            joinedload(Friendship.requester),
+        )
+
+
+    @staticmethod
+    def add_friend_requet(db: Session,requester_id: uuid.UUID,accepter_id: uuid.UUID) -> Friendship:
+        """
+        Создает новый запрос на дружбу со статусом PENDING.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            requester_id (uuid.UUID): ID пользователя, который отправляет запрос.
+            accepter_id (uuid.UUID): ID пользователя, который получает запрос.
+
+        Returns:
+            Friendship: Созданный объект Friendship.
+        """
+        new_friendship = Friendship(
+            requester_id=requester_id,
+            accepter_id=accepter_id,
+        )
+        db.add(new_friendship)
+        db.flush()
+        return new_friendship
+    
+    
+    @staticmethod
+    def update_friendship_status(db: Session,friendship_id: uuid.UUID,new_status: FriendshipStatus,accepted_at: datetime | None = None) -> Friendship | None:
+        """
+        Обновляет статус существующего запроса на дружбу.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            friendship_id (uuid.UUID): ID записи о дружбе.
+            new_status (FriendshipStatus): Новый статус (ACCEPTED или DECLINED).
+
+        Returns:
+            Optional[Friendship]: Обновленный объект Friendship или None, если не найден.
+        """
+        stmt = update(Friendship).where(
+            Friendship.id == friendship_id,
+        ).values(status=new_status,accepted_at=accepted_at)
+        result = db.execute(stmt)
+        return result.scalar_one_or_none()
+    
+
+    @staticmethod
+    def delete_friendship(db: Session,friendship_id: uuid.UUID) -> bool:
+        """
+        Удаляет запись о дружбе (или запрос) по её ID.
+
+        Args:
+            db (Session): Сессия базы данных SQLAlchemy.
+            friendship_id (uuid.UUID): ID записи о дружбе для удаления.
+
+        Returns:
+            bool: True, если запись была успешно удалена, иначе False.
+        """
+        stmt = delete(Friendship).where(
+            Friendship.id == friendship_id,
+        )
+        result = db.execute(stmt)
+        return result.rowcount > 0
