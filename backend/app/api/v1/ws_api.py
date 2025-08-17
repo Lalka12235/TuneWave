ab780666f201d9607296f94f8b4f7c17edd1be52
Egor <fisher12235@mail.ru>
2025-08-17 12:56:26 +0700

    ws api add globa room id

--- backend/app/api/v1/ws_api.py
+++ backend/app/api/v1/ws_api.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from app.ws.connection_manager import manager
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect,Depends
+from app.ws.connection_manager import manager,GLOBAL_ROOM_ID
 import uuid
 
 ws = APIRouter(
@@ -7,11 +7,21 @@ ws = APIRouter(
     prefix='/ws'
 )
 
-@ws.websocket("/room/{room_id}")
-async def websocket_endpoint(websocket: WebSocket, room_id: uuid.UUID):
-    await manager.connect(room_id, websocket)
+
+@ws.websocket("/room/{room_id}/{user_id}")
+async def websocket_endpoint(websocket: WebSocket, room_id: uuid.UUID,user_id: uuid.UUID):
+    await manager.connect(GLOBAL_ROOM_ID,websocket)
+    await manager.connect(room_id,user_id, websocket)
+    if room_id != GLOBAL_ROOM_ID: 
+        await manager.connect(room_id, user_id, websocket)
     try:
         while True:
             await websocket.receive_text()
     except WebSocketDisconnect:
-        manager.disconnect(room_id, websocket)
+        manager.disconnect(GLOBAL_ROOM_ID, user_id, websocket)
+        if room_id != GLOBAL_ROOM_ID:
+            manager.disconnect(room_id, user_id, websocket)
+    except Exception as e:
+        manager.disconnect(GLOBAL_ROOM_ID, user_id, websocket)
+        if room_id != GLOBAL_ROOM_ID:
+            manager.disconnect(room_id, user_id, websocket)
