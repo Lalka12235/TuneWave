fa3de467b1be39fc110a98398d0fcc9e0f008bd1
Lalka12235 <fisher12235@mail.ru>
2026-03-02 16:27:43 +0700

    feat: delete useless mappers

--- app/application/mappers/user_mapper.py
+++
@@ -1,7 +0,0 @@
-from app.domain.entity import UserEntity
-from app.presentation.schemas.user_schemas import UserResponse
-
-
-class UserMapper:
-    def to_response(self, user: UserEntity) -> UserResponse:
-        return UserResponse.model_validate(user)
